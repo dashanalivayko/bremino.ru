91d3ae237f1df6f10ee4543ab0dffbc16edc67a8
Lxpvpaeoo <lxpvpaeoo@gmail.com>
2021-04-21 11:08:16 +0300

    about_page_21.04.2021

--- my-app/src/components/pages/about/about.js
+++ my-app/src/components/pages/about/about.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import Banner from '../../common/banner/banner'
-import Map from '../../common/map/map'
+import Banner from '../../common/banner/banner';
+import Map from '../../common/map/map';
 
-import Gallery from '../../common/gallery/gallery'
+import Gallery from '../../common/gallery/gallery';
 
-import play from '../../../assets/iconplay.svg'
-import label from '../../../assets/labling.svg'
-import warehouse from '../../../assets/warehouse.svg'
-import truck from '../../../assets/cross-docking.svg'
-import shelf from '../../../assets/shelf.svg'
-import orsha from '../../../assets/breminoorsha.png'
+import {img} from '../../../assets/img';
 
 
 import './about.scss';
@@ -26,7 +21,7 @@ function About() {
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text  </Banner>
             <div className="wrapper">
                 <div className="about">
-                    <div className="about__cntainer">
+                    <div className="about__container">
                         <div className="about__items">
                             <p className="about__items-title">
                                 О нас
@@ -38,20 +33,20 @@ function About() {
                                 Главная цель — создание уникальных решений в сфере логистики для Вашего бизнеса.
                         </p>
                             <div className="about__items-link">
-                                <img src={play} className="about__items-iconplay" alt="logo" />
+                                <img src={img.play} className="about__items-iconplay" alt="logo" />
                                 <p className="about__items-link--video">посмотреть видео</p>
                             </div>
                         </div>
                         <div className="about__behold">
                             <div className="about__behold-box">
                                 <div className="about__behold-item">
-                                    <img src={label} className="about__behold-icon" alt="logo" />
+                                    <img src={img.label} className="about__behold-icon" alt="logo" />
                                     <p className="about__behols-title">
                                         Более 135 млн. промаркированных eдиниц товаров
                                 </p>
                                 </div>
                                 <div className="about__behold-item">
-                                    <img src={warehouse} className="about__behold-icon" alt="logo" />
+                                    <img src={img.warehouse} className="about__behold-icon" alt="logo" />
                                     <p className="about__behols-title">
                                         Более 135 млн. промаркированных eдиниц товаров
                                 </p>
@@ -59,13 +54,13 @@ function About() {
                             </div>
                             <div className="about__behold-box">
                                 <div className="about__behold-item">
-                                    <img src={truck} className="about__behold-icon" alt="logo" />
+                                    <img src={img.truck} className="about__behold-icon" alt="logo" />
                                     <p className="about__behols-title">
                                         Более 135 млн. промаркированных eдиниц товаров
                                 </p>
                                 </div>
                                 <div className="about__behold-item">
-                                    <img src={shelf} className="about__behold-icon" alt="logo" />
+                                    <img src={img.shelf} className="about__behold-icon" alt="logo" />
                                     <p className="about__behols-title">
                                         Более 135 млн. промаркированных eдиниц товаров
                                 </p>
@@ -85,7 +80,7 @@ function About() {
                         <p className="about__complex-text">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </p>
-                        <img src={orsha} className="about__items-iconplay" alt="logo" />
+                        <img src={img.orsha} className="about__items-img" alt="logo" />
                     </div>
                 </div>
                 <div className="about__complex">
@@ -96,7 +91,7 @@ function About() {
                         транспортно-логистический центр
                         </h2>
                     <div className="about__complex-desc">
-                        <img src={orsha} className="about__items-iconplay" alt="logo" />
+                        <img src={img.orsha} className="about__items-img" alt="logo" />
                         <p className="about__complex-text">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </p>
@@ -114,7 +109,7 @@ function About() {
                         <p className="about__complex-text">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </p>
-                        <img src={orsha} className="about__items-iconplay" alt="logo" />
+                        <img src={img.orsha} className="about__items-img" alt="logo" />
                     </div>
                 </div>
             </div>
