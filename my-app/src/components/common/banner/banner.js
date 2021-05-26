f1586ccce08d2fa7ca7187dd5233f46ce29ecc3d
Lxpvpaeoo <lxpvpaeoo@gmail.com>
2021-05-26 17:04:23 +0300

    UPD_banner_slider

--- my-app/src/components/common/banner/banner.js
+++ my-app/src/components/common/banner/banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Text from './text'
-import {TransparentButton} from '../../common/buttons/buttons'
+
 
 import './banner.scss';
 
@@ -8,24 +8,24 @@ import './banner.scss';
 function Banner(props) {
     return (
         <div>
-            <div className="banner">
+            <section className="banner">
                 <div className="wrapper">
                     <div className="banner__content">
                         <div className="banner__content-video">
                             {props.contentVideo}
                         </div>
+                        
                         <div className="banner__content-item">
                         <Text contentName={props.contentName}
                               contentTitle={props.contentTitle}>                      
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text </Text>
-                            <TransparentButton className="transparent-banner">заказать звонок</TransparentButton>
+                            
                         </div>
                         
                             
                     </div>
                 </div>
-
-            </div>
+            </section>
         </div>
     );
   }
--- my-app/src/components/common/banner/text.js
+++ my-app/src/components/common/banner/text.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './banner.scss';
-
+import {TransparentButton} from '../../common/buttons/buttons'
 
 function Text(props) {
     return (
-        <div className="banner__content-text">
-            <p className="banner__content-name">
-                {props.contentName}
-            </p>
-            <h1 className="banner__content-title">
-                {props.contentTitle}
-            </h1>
-            <h2 className="banner__content-subtitle">
-                {props.children}
-            </h2>
+        <div className="banner__content-text">    
+                <p className="banner__content-name">
+                    {props.contentName}
+                </p>
+            
+            <div className="banner__content-block">
+                <h1 className="banner__content-title">
+                    {props.contentTitle}
+                </h1>
+                <h2 className="banner__content-subtitle">
+                    {props.children}
+                </h2>
+                <TransparentButton className="transparent-banner">заказать звонок</TransparentButton>
+            </div>
+
         </div>
     );
 }
